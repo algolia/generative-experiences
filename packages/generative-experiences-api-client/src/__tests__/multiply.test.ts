8d36cd347cee8a8d6c9c10c407e869870292c284
Raed <raed.chammam@algolia.com>
2024-10-03 17:15:46 +0200

    remove broken test

--- packages/generative-experiences-api-client/src/__tests__/multiply.test.ts
+++ packages/generative-experiences-api-client/src/__tests__/multiply.test.ts
@@ -5,7 +5,3 @@ import { multiply } from '../multiply';
 test('multiply 2 * 2 to equal 4', () => {
   expect(multiply(2, 2)).toBe(4);
 });
-
-test('this should fail', () => {
-  expect(multiply(1, 1)).toBe(42);
-});
