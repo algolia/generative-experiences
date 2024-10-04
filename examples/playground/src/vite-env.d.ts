5be6079a0e8b1cbce12896bdb9aa253a321353be
Maria Lungu <heymarialungu@gmail.com>
2024-10-04 11:52:30 +0200

    update client options

--- examples/playground/src/vite-env.d.ts
+++ examples/playground/src/vite-env.d.ts
@@ -1,10 +1,11 @@
 interface ImportMetaEnv {
-  readonly VITE_EXAMPLES_APP_ID: string;
-  readonly VITE_EXAMPLES_MAIN_INDEX_NAME: string;
-  readonly VITE_EXAMPLES_API_KEY: string;
-  readonly VITE_EXAMPLES_GUIDES_API_KEY: string;
+    readonly VITE_EXAMPLES_APP_ID: string;
+    readonly VITE_EXAMPLES_MAIN_INDEX_NAME: string;
+    readonly VITE_EXAMPLES_API_KEY: string;
+    readonly VITE_EXAMPLES_GUIDES_API_KEY: string;
+    readonly VITE_EXAMPLES_GUIDES_INDEX_NAME: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+    readonly env: ImportMetaEnv;
 }
