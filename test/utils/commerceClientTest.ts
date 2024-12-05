0e04e57e165980d07413726dc68d50b5b7870d88
Maria Lungu <heymarialungu@gmail.com>
2024-12-05 15:25:49 +0100

    add test utils

---
+++ test/utils/commerceClientTest.ts
@@ -0,0 +1,33 @@
+import { CommerceClient } from '@algolia/generative-experiences-api-client';
+import { vi } from 'vitest';
+import { GeneratedHeadline, SingleHeadline } from './mockData';
+
+export function createTestClient(args: any = {}): CommerceClient {
+  return {
+    addAlgoliaAgent: vi.fn(),
+    searchSingleIndex: vi.fn(),
+    clearCache: vi.fn(),
+    options: {
+      host: '',
+      appId: '',
+      searchOnlyAPIKey: '',
+      writeAPIKey: '',
+      indexName: '',
+    },
+    appId: '',
+    getHeadlines: vi.fn(() => Promise.resolve(SingleHeadline)),
+    generateHeadlines: vi.fn(() =>
+      Promise.resolve({ data: GeneratedHeadline })
+    ),
+    transporter: {
+      userAgent: {
+        value: '',
+        add() {
+          return {};
+        },
+      },
+      queryParameters: {},
+    } as any,
+    ...args,
+  };
+}
---
+++ test/utils/mockData.ts
@@ -0,0 +1,23 @@
+export const SingleHeadline = [
+  {
+    objectID: '1',
+    title: 'Headline',
+    description: 'Test Headlines',
+    objects: [],
+    category: 'category-headline',
+    status: 'draft',
+    score_headline: 0,
+  },
+];
+
+export const GeneratedHeadline = [
+  {
+    objectID: '1',
+    title: 'Generated Headline',
+    description: 'Test Generated Headlines',
+    objects: [],
+    category: 'category-headline',
+    status: 'draft',
+    score_headline: 0,
+  },
+];
