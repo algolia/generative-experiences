c9b986cedadc4b48f9a969da830abbd26930932a
Raed <raed.chammam@algolia.com>
2025-01-10 10:16:33 +0100

    fix: re-export types from vdom [CEC-361] (#38)

--- packages/generative-experiences-js/src/index.ts
+++ packages/generative-experiences-js/src/index.ts
@@ -1,3 +1,4 @@
+export type * from '@algolia/generative-experiences-vdom';
 export * from './shoppingGuidesContent';
 export * from './shoppingGuidesFeedback';
 export * from './shoppingGuidesHeadlines';
